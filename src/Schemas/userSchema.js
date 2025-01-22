cddf5fecd926f008ebbec89a2e77dbf790639fe3
Rafael Soares <rafael.soares@amigotech.com.br>
2025-01-22 10:14:38 -0300

    modificações no schema de usuário

--- src/Schemas/userSchema.js
+++ src/Schemas/userSchema.js
@@ -1,12 +1,23 @@
 const yup = require("yup");
 
 const userSchema = {
-	storeUser:
-		yup.object().shape({
-		name: yup.string().min(4).max(30).required("É necessário inserir o nome do usuário."),
-		email: yup.string().min(16).max(30).required("É necessário informar um email."),
-		password: yup.string().min(6).required("É necessário informar uma senha."),
-		}).noUnknown(),
-}
-console.log(userSchema);
+  storeUser: yup
+    .object()
+    .shape({
+      name: yup
+        .string()
+        .min(4)
+        .max(30)
+        .required("É necessário inserir o nome do usuário."),
+      email: yup
+		  .string()
+		  .email("Necessário fornecer um email válido")
+        .min(16)
+        .max(30)
+        .required("É necessário informar um email."),
+      password: yup.string().min(6).required("É necessário informar uma senha.")
+    })
+    .noUnknown(true, "O objeto contém campos não permitidos")
+};
+
 module.exports = userSchema;
--- src/middlewares/validateSchemas.js
+++ src/middlewares/validateSchemas.js
@@ -1,25 +1,24 @@
 const validateSchema = (schema) => {
   return async (req, res, next) => {
     try {
-      console.log(req.body);
+      console.log("Requisição recebida:", req.body);
+
       if (req.body) {
-        req.body = await schema.validate(req.body, {
-          abortEarly: false
-        });
+        await schema.validate(req.body, { abortEarly: false });
+        console.log("funcionou!");
       }
       if (req.params) {
-        req.params = await schema.validate(req.params, {
-          abortEarly: false
-        });
+        await schema.validate(req.params, { abortEarly: false });
       }
       if (req.query) {
-        req.query = await schema.validate(req.query, {
-          abortEarly: false
-        });
+        await schema.validate(req.query, { abortEarly: false });
       }
+
       return next();
     } catch (error) {
-      return res.status(400).json({ message: error });
+      return res.status(400).json({
+        errors: error.errors || ["Erro na validação dos dados"]
+      });
     }
   };
 };
